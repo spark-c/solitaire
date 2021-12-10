b09ebb19e7b21f0cb00f7b11fe9e91d86c3b7dc5
sparksie <cklsparks@gmail.com>
2021-12-10 02:59:57 -0600

    implemented adding cards to a Stack and peeking contents

--- src/stack.py
+++ src/stack.py
@@ -4,8 +4,17 @@ from src.card import Card
 
 class Stack():
 
-    def __init__(self) -> None:
-        self.contents: List[Card|None] = list()
+    def __init__(self, input=None) -> None:
+        # default behavior
+        if input is None:
+            self.contents: list = list()
+        # we should be getting a List[Card] as input
+        elif type(input) is not list:
+            raise TypeError("Argument 'contents' must be type 'list'")
+        # input is a list; now we just hope that it contains Cards 
+        # TODO: let's find a nice way to futher ensure that ^^^
+        else:
+            self.contents: list = input
 
 
     @property
@@ -13,5 +22,36 @@ class Stack():
         return len(self.contents)
 
     
-    def add_card(card, to="top") -> None:
-        pass
+    def add_cards(self, input:List[Card], *, to:str="top") -> None:
+        """
+        Accepts a list of Cards to add to the Stack.
+        to="top" will place those cards at the end of the list, i.e. physically atop the existing stack.
+        to="bottom" will place those cards at the beginning of the list, i.e. physically underneath the stack.
+        """
+
+        if to not in ["top", "bottom"]:
+            print(f"{to=}")
+            raise ValueError("kwarg 'to' accepts 'top'(default) or 'bottom'.")
+        if type(input) is not list:
+            raise ValueError(f"Argument 'input' must be a list of Cards. (Received {type(input)}).")
+
+        if to == "top":
+            top: List[Card] = input
+            bottom: List[Card] = self.contents
+        else:
+            top: List[Card] = self.contents
+            bottom: List[Card] = input
+
+        self.contents = bottom + top
+
+
+    def get_from_top(self, last=1) -> Card:
+        """ Returns the topmost card(s) from the Stack """
+        index: int = 0 - last
+        return self.contents[index]
+
+
+    def get_from_bottom(self, first=1) -> Card:
+        """ Returns the buried-most card(s) from the Stack. Not useful in standard Solitaire rules. """
+        index: int = 0 + first
+        return self.contents[index]
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,5 +1,6 @@
 import unittest
 from src.stack import Stack
+from src.card import Card
 
 
 class TestStack(unittest.TestCase):
@@ -7,6 +8,29 @@ class TestStack(unittest.TestCase):
     def test_starts_empty(self):
         s = Stack()
         self.assertEqual(s.contents, list())
+
+
+    def test_add_card_to_top_of_stack(self):
+        s = Stack()
+        ace_hearts = Stack(input=[Card(1, "hearts")])
+        three_spades = Stack(input=[Card(3, "spades")])
+
+        s.add_cards(ace_hearts.contents)
+        s.add_cards(three_spades.contents)
+
+        self.assertEqual(s.contents[-1], three_spades.contents[0])
+
+
+    def test_add_card_to_bottom_of_stack(self):
+        s = Stack()        
+        ace_hearts = Stack(input=[Card(1, "hearts")])
+        three_spades = Stack(input=[Card(3, "spades")])
+
+
+        s.add_cards(ace_hearts.contents)
+        s.add_cards(three_spades.contents, to="bottom")
+
+        self.assertEqual(s.contents[0], three_spades.contents[0])
 
 
 
