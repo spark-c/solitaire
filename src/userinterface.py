b7dd861b3338e81022472c4da20212a643059a3c
Collin Sparks <cklsparks@gmail.com>
2021-12-20 16:18:10 -0500

    implemented _get_input and _validate, and tests for former

--- src/userinterface.py
+++ src/userinterface.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from typing import List, Dict, Optional
+from src.stack import Stack
+from src.board import Board
 
 
 @dataclass
@@ -6,49 +9,74 @@ class UserInput:
     """ Represents one piece of input submitted by the user """
     raw: str
     cleaned: str
+    # TODO: maybe remove self.content
     content: tuple[str, str, int]|None = None # src, dest, amt
     _is_valid: bool = False
     err: str|None = None
-        
 
+        
 class UserInterface:
     """ Handles collecting, validating, and enacting user input/commands """
 
-    def __init__(self, board):
-        self.board = board
-        self.current_input: UserInput|None = None
+    def __init__(self, board: Board):
+        self.board: Board = board
+        self.current_input: Optional[UserInput] = None
+        self.KEYMAP: Dict[str, Stack] = {
+            "w": self.board.waste,
+            "1": self.board.tableau[0],
+            "2": self.board.tableau[1],
+            "3": self.board.tableau[2],
+            "4": self.board.tableau[3],
+            "5": self.board.tableau[4],
+            "6": self.board.tableau[5],
+            "7": self.board.tableau[6],
+            "8": self.board.foundations[0],
+            "9": self.board.foundations[1],
+            "0": self.board.foundations[2],
+            "-": self.board.foundations[3],
+        }
 
 
-    def _get_input(self) -> None:
+    def _get_input(self, manual_input:str|None=None) -> None:
         """ Prompts user for input, collects, and returns as UserInput """
-        raw_in: str = input()
-        for char in raw_in:
-            if not char.isalnum():
-                raw_in.replace(char, "")
-        cleaned: str = raw_in.lower()
+        if manual_input is not None:
+            raw_in:str = manual_input
+        else:
+            raw_in: str = input()
+
+        cleaning: List[str] = list(filter(lambda c: c.isalnum(), raw_in))
+        cleaned: str = "".join(cleaning)
         self.current_input = UserInput(raw=raw_in, cleaned=cleaned)
 
 
-    def _validate(self, input:UserInput) -> None:
+    def _validate(self, input:Optional[UserInput]) -> None:
         """ Checks that user input is in a valid format """
+        if input is None:
+            return
+
         cmd: str = input.cleaned
         if cmd == "help":
             input._is_valid = True
+            return
 
         if len(cmd) != 3:
             input._is_valid = False
             input.err = f"Too many or not enough arguments! You entered: {input.raw}"
+            return
 
-        # TODO: finish validate
+        input._is_valid = True
 
 
-    def _parse(self) -> tuple[str, str, int]:
-        """ Parses valid userinput into a tuple(src:str, dest:str, amt:int) """
-        return ("", "", 0)
+    def _parse(self) -> tuple[Stack, Stack, int]:
+        """ Parses valid userinput into a tuple(src:Stack, dest:Stack, amt:int) """
+        cmd: str = input.cleaned
+        return (self.KEYMAP[cmd[0]], self.KEYMAP[cmd[1]], int(cmd[2]))
 
 
     def _enact(self, user_input: tuple) -> None:
         """ Receives a valid command tuple and """
+        # try:
+            # self.board.move_cards()
         pass
 
 
--- tests/test_userinterface.py
+++ tests/test_userinterface.py
@@ -1,9 +1,34 @@
 import unittest
+from src.board import Board
+from src.userinterface import UserInterface
 
 
 class TestUserInterface(unittest.TestCase):
 
-    def test_something(self):
+    def setUp(self) -> None:
+        b = Board()
+        self.ui = UserInterface(b)
+        self.user_in = " hello!@#$%^&*()-=\\/.,';: `~world123 "
+
+
+    def test_get_input_removes_special_chars(self):
+        user_in = self.user_in
+        self.ui._get_input(manual_input=user_in)
+
+        self.assertEqual(self.ui.current_input.cleaned, "helloworld123") #type:ignore
+
+
+    def test_get_input_preserves_original_input(self):
+        user_in = self.user_in
+        self.ui._get_input(manual_input=user_in)
+
+        self.assertEqual(self.ui.current_input.raw, user_in) #type:ignore
+
+
+    def test_validate_accepts_help(self):
+        # user_in = self.user_in
+        # self.ui._get_input(manual_input=user_in)
+        # self.ui._validate(self.ui.current_input)
         pass
 
 
